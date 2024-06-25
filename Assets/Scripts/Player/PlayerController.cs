d9b8196687c28accfbefa1f717f55aa34d78bbaf
Samuel Ting <tingjs05@gmail.com>
2024-06-25 17:26:09 +0800

    added attack range

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class PlayerController : StateMachine<PlayerController>
 {
+    #region Inspector Fields
+    [SerializeField] float attackRange = 1.5f;
+    [SerializeField] bool showGizmos = true;
+    #endregion
+
     #region States
     public PlayerDefaultState DefaultState { get; private set; }
     public PlayerAttackState AttackState { get; private set; }
@@ -11,6 +16,7 @@ public class PlayerController : StateMachine<PlayerController>
 
     #region Other Properties
     public PlayerCharacter Data { get; private set; }
+    public Transform pointer => transform.GetChild(0);
     #endregion
 
     #region Components
@@ -40,6 +46,13 @@ public class PlayerController : StateMachine<PlayerController>
     void OnCharacterChange(PlayerCharacter data)
     {
         Data = data;
+    }
+    #endregion
+
+    #region Gizmos
+    void OnDrawGizmosSelected() 
+    {
+        Gizmos.DrawWireSphere(pointer.position, attackRange);
     }
     #endregion
 }
