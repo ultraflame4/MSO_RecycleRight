e21da43b739bf08935c16c9d4e6a4a63329317e6
ultraflame42 <34125174+ultraflame4@users.noreply.github.com>
2024-06-27 13:44:18 +0800

    added navigation

--- Assets/Scripts/NPCs/Navigation.cs
+++ Assets/Scripts/NPCs/Navigation.cs
@@ -4,8 +4,26 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class Navigation : MonoBehaviour
 {
+
+    [Tooltip("When distance between target & this game object is less than stop_distance, it will count as reached destination.")]
+    public float stop_distance = 0.1f;
+    [Tooltip("The movement speed")]
+    public float move_speed = 100f;
+
+    private Rigidbody2D rb;
     private Transform target;     
     private Vector3? current_target_pos;
+
+    private void Start() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    public bool reachedDestination {
+        get {
+            if (current_target_pos == null) return false;
+            return (current_target_pos.Value - transform.position).sqrMagnitude < stop_distance * stop_distance;
+        }
+    }
 
 
     public void SetDestination(Vector3 pos)
@@ -20,7 +38,7 @@ public class Navigation : MonoBehaviour
 
     private void Update()
     {
-        if (target == null)
+        if (target != null)
         {
             current_target_pos = target.position;
         }
@@ -33,7 +51,18 @@ public class Navigation : MonoBehaviour
 
     private void MoveToStep(Vector3 pos)
     {
-        // todo change this later
-        transform.position = Vector3.Lerp(transform.position, pos, Time.deltaTime);
+        Vector3 dir = pos - transform.position;
+        Vector3 vel = dir.normalized * move_speed * Time.deltaTime;
+        rb.velocity = vel * Mathf.Clamp01( dir.sqrMagnitude);
+        
+        if (reachedDestination) return;
+        rb.velocity = Vector2.zero;
+    }
+
+    private void OnDrawGizmosSelected() {
+        if (current_target_pos == null) return;
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(current_target_pos.Value, stop_distance);
+        Gizmos.DrawLine(transform.position, current_target_pos.Value);
     }
 }
