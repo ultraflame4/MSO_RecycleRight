c85e81ef6afbd11498f7e11b0e2ffa63f7802551
Samuel Ting <tingjs05@gmail.com>
2024-07-02 13:17:19 +0800

    fixed pivot

--- Assets/Scripts/Player/DirectionPointer.cs
+++ Assets/Scripts/Player/DirectionPointer.cs
@@ -5,6 +5,8 @@ namespace Player
     public class DirectionPointer : MonoBehaviour {
         [field: SerializeField, Tooltip("How far away the pointer should be away from the player.")]
         public float distance { get; private set; }
+        [field: SerializeField, Tooltip("The player's height.")]
+        public float height { get; private set; }
         [field: SerializeField, Tooltip("The player's transform.")]
         public Transform player { get; private set; }
 
@@ -15,10 +17,12 @@ namespace Player
             // Ensure that comparison is done in 2D space
             player_pos.z = 0; 
             mouse_position.z = 0; 
-
+            // offset player position by height
+            player_pos.y += height;
+            // set direction of mouse pointer to the player
             Vector3 direction = (mouse_position - player_pos).normalized;
-
-            transform.position = player.position + direction * distance;
+            // offset pointer from player's position + height
+            transform.position = player_pos + direction * distance;
             // Rotate the pointer to face the mouse
             transform.up = direction;
         }
