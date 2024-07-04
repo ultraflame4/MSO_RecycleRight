473e99f7d095dcacb1097b889df51c25da82be3b
ultraflame42 <34125174+ultraflame4@users.noreply.github.com>
2024-07-04 10:24:13 +0800

    contaminant can now contaminant recyclables

--- Assets/Scripts/NPCs/Recyclables/RecyclableNPC.cs
+++ Assets/Scripts/NPCs/Recyclables/RecyclableNPC.cs
@@ -20,11 +20,11 @@ namespace NPC.Recyclable
         #region Config
         [SerializeField]
         private RecyclableType _recyclableType;
-        public override RecyclableType recyclableType => _recyclableType;
+        public GameObject contaminant_prefab;
         public float sightRange = 3f;
         #endregion
 
-
+        public override RecyclableType recyclableType => _recyclableType;
 
         public ContaminantNPC nearestContaminant { get; private set; } = null;
 
@@ -38,7 +38,10 @@ namespace NPC.Recyclable
         }
         public void Contaminate(float damage)
         {
-            Debug.Log("Hit by Contaminant!");
+            // Debug.Log("Hit by Contaminant!");
+            var contaminant = Instantiate(contaminant_prefab);
+            contaminant.transform.position = transform.position;
+            Destroy(gameObject);
         }
 
         public void Stun(float stun_duration)
