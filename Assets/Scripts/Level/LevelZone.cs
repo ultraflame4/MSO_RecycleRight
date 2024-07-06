be3b0a905feccd5070f6610f07fd60f55af357b5
ultr42 <ultr42@skiff.com>
2024-07-06 12:54:14 +0800

    allow offsets for level zones

--- Assets/Scripts/Level/LevelZone.cs
+++ Assets/Scripts/Level/LevelZone.cs
@@ -6,14 +6,22 @@ namespace Level
     {
         [field: SerializeField]
         public Vector2 size { get; private set; } = new Vector2(20, 10);
-        [field: SerializeField]
+        [field: SerializeField, Tooltip("How much to offset the zone. Should not affect camera positioning.")]
+        public Vector2 offset { get; private set; } = Vector2.zero;
+        [field: SerializeField, Tooltip("The buffer zone size. This is mainly used for NPC Ai Navigation")]
         public float buffer_zone_size { get; private set; } = 10f;
 
-
-        [field: SerializeField]
+        [field: SerializeField, Tooltip("Determines where the player should start in the zone. This is the offset from the left edge of the zone.")]
         public float player_start_offset { get; private set; } = 1;
-        public float player_startpos_x => transform.position.x - size.x / 2 + player_start_offset;
-
+        public float player_startpos_x => center.x - size.x / 2 + player_start_offset;
+        /// <summary>
+        /// The center of the zone
+        /// </summary>
+        public Vector3 center=> transform.position + (Vector3)offset;
+        /// <summary>
+        /// The position the camera should target
+        /// </summary>
+        public Vector2 camera_target_pos=> transform.position;
         public ILevelEntity[] entities;
 
         private void Start()
@@ -31,21 +39,21 @@ namespace Level
         }
         public bool PositionWithinZone(Vector3 position)
         {
-            bool x = position.x >= transform.position.x - size.x / 2 && position.x <= transform.position.x + size.x / 2;
-            bool y = position.y >= transform.position.y - size.y / 2 && position.y <= transform.position.y + size.y / 2;
+            bool x = position.x >= center.x - size.x / 2 && position.x <= center.x + size.x / 2;
+            bool y = position.y >= center.y - size.y / 2 && position.y <= center.y + size.y / 2;
             return x && y;
         }
 
         public float DistanceFromEdgeX(Vector3 position)
         {
-            float leftEdge = transform.position.x - size.x / 2;
-            float rightEdge = transform.position.x + size.x / 2;
+            float leftEdge = center.x - size.x / 2;
+            float rightEdge = center.x + size.x / 2;
             return Mathf.Min(position.x - leftEdge, rightEdge - position.x);
         }
         public float DistanceFromEdgeY(Vector3 position)
         {
-            float bottomEdge = transform.position.y - size.y / 2;
-            float topEdge = transform.position.y + size.y / 2;
+            float bottomEdge = center.y - size.y / 2;
+            float topEdge = center.y + size.y / 2;
             return Mathf.Min(position.y - bottomEdge, topEdge - position.y);
         }
 
@@ -63,14 +71,14 @@ namespace Level
         public void OnDrawGizmos()
         {
             Gizmos.color = Color.white;
-            Gizmos.DrawWireCube(transform.position, size);
+            Gizmos.DrawWireCube(center, size);
             Gizmos.color = Color.yellow;
-            Gizmos.DrawWireCube(transform.position, size - Vector2.one * buffer_zone_size * 2);
+            Gizmos.DrawWireCube(center, size - Vector2.one * buffer_zone_size * 2);
             Gizmos.color = Color.cyan * .1f;
-            Gizmos.DrawCube(transform.position, size);
+            Gizmos.DrawCube(center, size);
 
             Gizmos.color = Color.green;
-            Gizmos.DrawLine(new Vector2(player_startpos_x, transform.position.y - size.y / 2), new Vector2(player_startpos_x, transform.position.y + size.y / 2));
+            Gizmos.DrawLine(new Vector2(player_startpos_x, center.y - (size.y) / 2), new Vector2(player_startpos_x, center.y + size.y / 2));
         }
     }
 }
