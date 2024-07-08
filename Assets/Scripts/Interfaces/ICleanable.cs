80c9eba64958c243308dd7afb5c0f897405fc63b
ultraflame42 <34125174+ultraflame4@users.noreply.github.com>
2024-07-08 13:50:31 +0800

    Added comments

--- Assets/Scripts/Interfaces/ICleanable.cs
+++ Assets/Scripts/Interfaces/ICleanable.cs
@@ -1,7 +1,14 @@
 namespace Interfaces
 {
+    /// <summary>
+    /// Objects that implemented this can be cleaned
+    /// </summary>
     public interface ICleanable
     {
+        /// <summary>
+        /// Cleans this object by a certain amount.
+        /// </summary>
+        /// <param name="clean_amount">The amount to clean (Percentage ranging from 0.0 - 1.0)</param>
         public void Clean(float clean_amount);
     }
 }
--- Assets/Scripts/Interfaces/IDamagable.cs
+++ Assets/Scripts/Interfaces/IDamagable.cs
@@ -1,7 +1,14 @@
 namespace Interfaces
 {
+    /// <summary>
+    /// Objects that implement this can be damaged.
+    /// </summary>
     public interface IDamagable
     {
+        /// <summary>
+        /// Damages this entity by a certain amount
+        /// </summary>
+        /// <param name="damage">damage amount.</param>
         public void Damage(float damage);
     }
 }
