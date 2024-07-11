3afb29812fc3b72e96be51320fe616c9e5da5c37
Samuel Ting <tingjs05@gmail.com>
2024-07-11 11:06:54 +0800

    tutorial task class

--- Assets/Scripts/Level/Managers/Tutorial/TutorialManager.cs
+++ Assets/Scripts/Level/Managers/Tutorial/TutorialManager.cs
@@ -1,13 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using Level;
 using UnityEngine;
 
 public class TutorialManager : MonoBehaviour
 {
+    TutorialTask[][] tasks;
+    LevelManager levelManager;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        levelManager = LevelManager.Instance;
+        tasks = new TutorialTask[levelManager.zones.Length][];
     }
 
     // Update is called once per frame
---
+++ Assets/Scripts/Level/Managers/Tutorial/TutorialTask.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+public abstract class TutorialTask : MonoBehaviour
+{
+    [Tooltip("Minimum duration to keep tutorial active.")]
+    [SerializeField] float minCompletionDuration = 5f;
+    [Tooltip("UI elements to show for the tutorial.")]
+    [SerializeField] GameObject[] UIelements;
+    Coroutine coroutine;
+
+    // public boolean properties
+    public bool IsActive { get; private set; }
+    public bool IsCompleted { get; private set; }
+
+    // event to be called when task is marked as completed
+    public event Action TaskCompleted;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // reset coroutine
+        coroutine = null;
+        // reset properties
+        IsActive = false;
+        IsCompleted = false;
+        // when game is starting, disable UI elements
+        SetTutorialActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!CheckTaskCompletion() || coroutine != null) return;
+        coroutine = StartCoroutine(CountDuration());
+    }
+
+    public void SetTutorialActive(bool active)
+    {
+        // set active of UI element objects
+        foreach(GameObject obj in UIelements)
+        {
+            obj.SetActive(active);
+        }
+        // update is active property
+        IsActive = active;
+    }
+
+    public abstract bool CheckTaskCompletion();
+
+    IEnumerator CountDuration()
+    {
+        yield return new WaitForSeconds(minCompletionDuration);
+        // reset tutorial
+        SetTutorialActive(false);
+        coroutine = null;
+        // mark tutorial as completed
+        IsCompleted = true;
+        // invoke event
+        TaskCompleted?.Invoke();
+    }
+}
