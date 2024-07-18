73f2a5644a292a741a7e8dfc4f7767df4296f4d7
ultraflame42 <34125174+ultraflame4@users.noreply.github.com>
2024-07-18 10:57:17 +0800

    created component level info

--- Assets/Scripts/Level/LevelInfo.cs
+++ Assets/Scripts/Level/LevelInfo.cs
@@ -1,30 +1,5 @@
 using UnityEngine;
 
-/// <summary>
-/// Information about the npcs in the level.
-/// </summary>
-public struct LevelNPC{
-    /// <summary>
-    /// The data about the level npc
-    /// </summary>
-    TrashNpcSO npcData;
-    /// <summary>
-    /// Whether the npc spawns as a contaminant in the level
-    /// </summary>
-    bool is_contaminant;
-    /// <summary>
-    /// Number of npcs of this type.
-    /// </summary>
-    int count;
-}
-
-/// <summary>
-/// Scriptable object to hold information about the level.
-/// </summary>
-[CreateAssetMenu(fileName = "LevelInfo", menuName = "LevelInfo", order = 0)]
-public class LevelInfo : ScriptableObject {
-    public string levelName;
-    public string levelDescription;
-    public float maxScore;
-    public LevelNPC[] npcs;
+public class LevelInfo : MonoBehaviour {
+    public LevelInfoSO info;
 }
