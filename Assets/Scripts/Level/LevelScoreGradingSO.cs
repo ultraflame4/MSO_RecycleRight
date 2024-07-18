562d7a2a1b904830b033f39e330e4a79556b62e6
ultr42 <ultr42@skiff.com>
2024-07-18 20:40:46 +0800

    added scores to game manager

--- Assets/Scripts/Level/LevelScoreGradingSO.cs
+++ Assets/Scripts/Level/LevelScoreGradingSO.cs
@@ -1,6 +1,7 @@
+using System;
 using UnityEngine;
 
-
+[Serializable]
 public struct LevelScoreGrade
 {
     public string grade;
@@ -10,10 +11,21 @@ public struct LevelScoreGrade
     public Sprite gradeSprite;
     [Tooltip("The number of times to repeat the sprite (horizontal).")]
     public int spriteRepeat;
-
 }
 
 [CreateAssetMenu(fileName = "LevelScoreGradingSO", menuName = "ScriptableObjects/Level/LevelScoreGradingSO", order = 0)]
-public class LevelScoreGradingSO : ScriptableObject {
-    public LevelScoreGrade[] grades;
+public class LevelScoreGradingSO : ScriptableObject
+{
+    public LevelScoreGrade failGrades;
+    public LevelScoreGrade[] passGrades;
+
+    [EasyButtons.Button]
+    public void AutoSetPercentage()
+    {
+        float interval = 1f / (passGrades.Length-1);
+        for (int i = 0; i < passGrades.Length; i++)
+        {
+            passGrades[i].percentage = interval * i;
+        }
+    }
 }
--- Assets/Scripts/Level/Managers/GameManager.cs
+++ Assets/Scripts/Level/Managers/GameManager.cs
@@ -30,6 +30,7 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    public LevelScoreGradingSO levelScoreGradingSO;
 
     // singleton instance
     private static GameManager _instance;
