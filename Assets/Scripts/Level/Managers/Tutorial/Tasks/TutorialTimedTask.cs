b67f662601c3d5742724385055735d9eb5eafd7a
Samuel Ting <tingjs05@gmail.com>
2024-07-18 14:35:26 +0800

    timed task completed (not tested)

---
+++ Assets/Scripts/Level/Managers/Tutorial/Tasks/TutorialTimedTask.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Level.Tutorial
+{
+    public class TutorialTimedTask : TutorialButtonPressTask
+    {
+        [Header("Timed Task")]
+        [SerializeField] float duration = 2.5f;
+        float timePassed;
+
+        new void Start()
+        {
+            base.Start();
+            timePassed = 0f;
+        }
+
+        public override bool CheckTaskCompletion()
+        {
+            if (base.CheckTaskCompletion()) return true;
+            timePassed += Time.deltaTime;
+            return timePassed >= duration;
+        }
+    }
+}
