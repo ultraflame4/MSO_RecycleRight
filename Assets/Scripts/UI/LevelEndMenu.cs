0bf8091cea7fb1510f52cc1ee19625cddc0c8b6a
ultraflame42 <34125174+ultraflame4@users.noreply.github.com>
2024-07-18 15:59:11 +0800

    AHHHH

---
+++ Assets/Scripts/UI/LevelEndMenu.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class LevelEndMenu : MonoBehaviour {
+    public static void GradeFromScore(float score, float maxScore){
+        // todo, load grades (and their score from SO) Change grade to be sprites.
+    }
+}
