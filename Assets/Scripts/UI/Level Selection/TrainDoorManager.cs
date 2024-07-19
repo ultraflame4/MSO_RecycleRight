7c1fe9e3762a021ea1411961b6349713d0ba02eb
Samuel Ting <tingjs05@gmail.com>
2024-07-19 19:04:49 +0800

    door animation almost complete (movement still got smth funny)

--- Assets/Scripts/UI/Level Selection/TrainDoorManager.cs
+++ Assets/Scripts/UI/Level Selection/TrainDoorManager.cs
@@ -4,37 +4,87 @@ using UnityEngine;
 
 public class TrainDoorManager : MonoBehaviour
 {
+    [Header("Door")]
     [SerializeField] RectTransform leftDoor;
     [SerializeField] RectTransform rightDoor;
-
     [SerializeField, Range(0f, 1f)] float doorPosition = 1f;
-    public float DoorPosition
-    {
-        get { return doorPosition; }
-        set 
-        { 
-            doorPosition = Mathf.Clamp01(value); 
-            UpdateDoorPosition();
-        }
-    }
-
     [SerializeField] bool debug_update_door_position = false;
+
+    [Header("Movement")]
+    [SerializeField] float moveAnimationDuration = 2.5f;
+    [SerializeField] float endScale = .75f;
+    [SerializeField] Vector3 endOffset = Vector3.zero;
+    [SerializeField, Range(0f, 1f)] float moveToZoomAnimationRatio = 0.5f;
+    [SerializeField, Range(0f, 1f)] float animationProgress;
+    [SerializeField] bool debug_play = false;
 
     float originalDoorPosX;
     float screenWidth;
+    Coroutine moveAnimation;
 
     // Start is called before the first frame update
     void Start()
     {
         originalDoorPosX = rightDoor.localPosition.x;
         screenWidth = (Screen.width / 2f) + originalDoorPosX;
+        doorPosition = 0f;
+        animationProgress = 0f;
+        UpdateDoorPosition();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (debug_play) PlayAnimation();
         if (!debug_update_door_position) return;
         UpdateDoorPosition();
+    }
+
+    public void PlayAnimation()
+    {
+        if (moveAnimation != null) return;
+        moveAnimation = StartCoroutine(Animate(animationProgress <= 0f));
+    }
+
+    IEnumerator Animate(bool forward_direction)
+    {
+        float timeElapsed = 0f;
+
+        while (timeElapsed <= moveAnimationDuration)
+        {
+            timeElapsed += Time.deltaTime;
+            animationProgress = Mathf.Clamp01(forward_direction ? 
+                (timeElapsed / moveAnimationDuration) : 1f - (timeElapsed / moveAnimationDuration));
+            UpdateAnimation();
+            yield return timeElapsed;
+        }
+
+        transform.localScale = forward_direction ? new Vector3(endScale, endScale, endScale) : Vector3.one;
+        // transform.localPosition = forward_direction ? Vector3.zero + endOffset : Vector3.zero;
+        doorPosition = forward_direction ? 1f : 0f;
+        UpdateDoorPosition();
+        moveAnimation = null;
+        debug_play = false;
+    }
+
+
+    void UpdateAnimation()
+    {
+        if (animationProgress <= moveToZoomAnimationRatio)
+        {
+            doorPosition = animationProgress / moveToZoomAnimationRatio;
+            doorPosition = Mathf.Clamp01(doorPosition);
+            UpdateDoorPosition();
+            return;
+        }
+
+        float currentCycleRatio = (animationProgress - moveToZoomAnimationRatio) / (1f - moveToZoomAnimationRatio);
+        // change scale
+        float currentScale = endScale + ((1f - endScale) * (1f - currentCycleRatio));
+        transform.localScale = new Vector3(currentScale, currentScale, currentScale);
+        // change position
+        Vector3 currentPosition = transform.localPosition + endOffset + ((Vector3.one - (transform.localPosition + endOffset)) * (1f - currentCycleRatio));
+        transform.localPosition = currentPosition;
     }
 
     void UpdateDoorPosition()
