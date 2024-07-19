2d52cfa00dafaf4c66cc74710c196788ea0fdfec
ultr42 <ultr42@skiff.com>
2024-07-19 12:02:13 +0800

    added badges to ui

--- Assets/Scripts/Level/LevelScoreGradingSO.cs
+++ Assets/Scripts/Level/LevelScoreGradingSO.cs
@@ -5,12 +5,18 @@ using UnityEngine;
 public struct LevelScoreGrade
 {
     public string grade;
+
+    [Tooltip("The color to display for this grade. Used for text.")]
+    public Color gradeColor;
+    [Tooltip("The description to display for this grade.")]
+    public string gradeDescription;
     [Range(0, 1)]
     public float percentage;
     [Tooltip("The sprite to display for this grade.")]
     public Sprite gradeSprite;
     [Tooltip("The number of times to repeat the sprite (horizontal).")]
     public int spriteRepeat;
+
 }
 
 [CreateAssetMenu(fileName = "LevelScoreGradingSO", menuName = "ScriptableObjects/Level/LevelScoreGradingSO", order = 0)]
