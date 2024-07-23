cd4291d3a9ab53353233ca1b8147679b2ba55464
Samuel Ting <tingjs05@gmail.com>
2024-07-23 11:40:41 +0800

    moved pause manager into namespace, created level selection manager script

--- Assets/Scripts/UI/Gameplay/PauseManager.cs
+++ Assets/Scripts/UI/Gameplay/PauseManager.cs
@@ -2,77 +2,81 @@ using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class PauseManager : MonoBehaviour
+namespace UI
 {
-    [SerializeField] KeyCode pauseKey = KeyCode.Escape;
-    private GameObject pauseMenu;
-
-    public bool Paused { get; private set; } = false;
-
-    private static PauseManager _instance;
-    public static PauseManager Instance
+    public class PauseManager : MonoBehaviour
     {
-        get
+        [SerializeField] KeyCode pauseKey = KeyCode.Escape;
+        private GameObject pauseMenu;
+
+        public bool Paused { get; private set; } = false;
+
+        private static PauseManager _instance;
+        public static PauseManager Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    throw new NullReferenceException("There is no pause manager instance in the scene!");
+                }
+                return _instance;
+            }
+        }
+
+        void Awake()
         {
             if (_instance == null)
-            {
-                throw new NullReferenceException("There is no pause manager instance in the scene!");
-            }
-            return _instance;
+                _instance = this;
+            else
+                Debug.LogWarning("There are multiple PauseManager(s) in the scene! This is not allowed!");
         }
-    }
 
-    void Awake()
-    {
-        if (_instance == null)
-            _instance = this;
-        else
-            Debug.LogWarning("There are multiple PauseManager(s) in the scene! This is not allowed!");
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        pauseMenu = transform.GetChild(0).gameObject;
-        pauseMenu?.SetActive(false);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (!Input.GetKeyDown(pauseKey)) return;
-        TogglePause();
-    }
-
-    // menu button methods
-    public void TogglePause()
-    {
-        Paused = !Paused;
-        Time.timeScale = Paused ? 0f : 1f;
-        pauseMenu?.SetActive(Paused);
-    }
-
-    public void RestartLevel()
-    {
-        if (Paused) TogglePause();
-        LoadScene(SceneManager.GetActiveScene().name);
-    }
-
-    public void LeaveLevel(string scene_name)
-    {
-        if (Paused) TogglePause();
-        LoadScene(scene_name);
-    }
-
-    void LoadScene(string scene_name)
-    {
-        if (GameManager.Instance == null)
+        // Start is called before the first frame update
+        void Start()
         {
-            Debug.LogWarning("Game Manager instance could not be found, manually switching scenes instead. (PauseManager.cs)");
-            SceneManager.LoadScene(scene_name);
-            return;
+            pauseMenu = transform.GetChild(0).gameObject;
+            pauseMenu?.SetActive(false);
         }
 
-        GameManager.Instance.LoadScene(scene_name);
+        // Update is called once per frame
+        void Update()
+        {
+            if (!Input.GetKeyDown(pauseKey)) return;
+            TogglePause();
+        }
+
+        // menu button methods
+        public void TogglePause()
+        {
+            Paused = !Paused;
+            Time.timeScale = Paused ? 0f : 1f;
+            pauseMenu?.SetActive(Paused);
+        }
+
+        public void RestartLevel()
+        {
+            if (Paused) TogglePause();
+            LoadScene(SceneManager.GetActiveScene().name);
+        }
+
+        public void LeaveLevel(string scene_name)
+        {
+            if (Paused) TogglePause();
+            LoadScene(scene_name);
+        }
+
+        void LoadScene(string scene_name)
+        {
+            if (GameManager.Instance == null)
+            {
+                Debug.LogWarning("Game Manager instance could not be found, manually switching scenes instead. (PauseManager.cs)");
+                SceneManager.LoadScene(scene_name);
+                return;
+            }
+
+            GameManager.Instance.LoadScene(scene_name);
+        }
     }
 }
+
