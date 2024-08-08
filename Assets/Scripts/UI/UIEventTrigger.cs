d11db623075c9c4fd4ec3604324a6461ae62db8a
fybs-maker <linjingxiang.ljx@gmail.com>
2024-08-08 16:19:38 +0800

    Train door hover

--- Assets/Scripts/UI/UIEventTrigger.cs
+++ Assets/Scripts/UI/UIEventTrigger.cs
@@ -1,24 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UI;
 
 public class UIEventTrigger : MonoBehaviour
 {
     public GameObject btnStart;
+    public UIPointerHandler pointerHandler;
 
-    // Start is called before the first frame update
+
     void Start()
     {
-        
+        if (pointerHandler != null)
+        {
+            pointerHandler.PointerEnter += OnPointerEnter;
+            pointerHandler.PointerExit += OnPointerExit;
+            pointerHandler.PointerDown += OnPointerDown;
+        }
+        btnStart.GetComponent<Image>().enabled = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnPointerEnter()
     {
-        
+        btnStart.GetComponent<Image>().enabled = true;
+        Debug.Log("Pointer enter the UI element");
     }
-    public void Enter()
+
+    void OnPointerExit()
     {
-        btnStart.SetActive(true);
+        btnStart.GetComponent<Image>().enabled = false;
+        Debug.Log("Pointer exit the UI element");
+    }
+
+    void OnPointerDown()
+    {
+        Debug.Log("Pointer down on the UI element.");
     }
 }
+
+    
+    
+    
+    
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
