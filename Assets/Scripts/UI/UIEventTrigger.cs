bf9c9ee8a75875d07256f26118aaa520abff9a77
fybs-maker <linjingxiang.ljx@gmail.com>
2024-08-08 16:26:10 +0800

    Added comments to the script UIEventTrigger

--- Assets/Scripts/UI/UIEventTrigger.cs
+++ Assets/Scripts/UI/UIEventTrigger.cs
@@ -6,27 +6,31 @@ using UI;
 
 public class UIEventTrigger : MonoBehaviour
 {
+    //Create reference variable
     public GameObject btnStart;
     public UIPointerHandler pointerHandler;
 
 
     void Start()
     {
+        //Calls the event from UIPointerHandler script
         if (pointerHandler != null)
         {
             pointerHandler.PointerEnter += OnPointerEnter;
             pointerHandler.PointerExit += OnPointerExit;
             pointerHandler.PointerDown += OnPointerDown;
         }
+        //UI is hidden from start
         btnStart.GetComponent<Image>().enabled = false;
     }
 
+    //When pointer enter the UI element the UI shows up
     void OnPointerEnter()
     {
         btnStart.GetComponent<Image>().enabled = true;
         Debug.Log("Pointer enter the UI element");
     }
-
+    // When pointer exit the UI element it hides the UI
     void OnPointerExit()
     {
         btnStart.GetComponent<Image>().enabled = false;
