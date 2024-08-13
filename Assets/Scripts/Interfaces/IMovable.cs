c7b429320d22223ec64bd772f7033de47d22e03f
Samuel Ting <tingjs05@gmail.com>
2024-08-13 11:24:49 +0800

    added imovable interface

---
+++ Assets/Scripts/Interfaces/IMovable.cs
@@ -0,0 +1,14 @@
+namespace Interfaces
+{
+    /// <summary>
+    /// Objects that return this method can toggle between being able, or unable to be moved by players
+    /// </summary>
+    public interface IMovable
+    {
+        /// <summary>
+        /// Checks if the object can be moved
+        /// </summary>
+        /// <returns>Returns a boolean depending on if the object can be moved</returns>
+        public bool CanMove();
+    }
+}
