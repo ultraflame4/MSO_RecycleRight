d87830e1517560d6b1649d151527f4902dd5e79d
Samuel Ting <tingjs05@gmail.com>
2024-08-13 12:49:29 +0800

    bin bug appears to be fixed

--- Assets/Scripts/Bosses/Pilotras/States/BinDropState.cs
+++ Assets/Scripts/Bosses/Pilotras/States/BinDropState.cs
@@ -154,7 +154,8 @@ namespace Bosses.Pilotras.FSM
             // search for recycling type with the highest number of NPCs
             RecyclableType[] selectedTypes = selectedBins.Select(x => x.recyclableType).ToArray();
             RecyclableType maxType = character.data.npcCount
-                .Aggregate((l, r) => l.Value > r.Value && Array.IndexOf(selectedTypes, l.Key) == -1 ? l : r).Key;
+                .Where(x => Array.IndexOf(selectedTypes, x.Key) == -1)
+                .Aggregate((l, r) => l.Value > r.Value ? l : r).Key;
             
             Debug.Log($"maxType: {maxType}, count: {character.data.npcCount[maxType]}");
 
