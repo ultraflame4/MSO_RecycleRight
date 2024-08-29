a81184dea72e4d32c2cd3e12bc06b8a0eb4a6dab
Samuel Ting <tingjs05@gmail.com>
2024-08-29 15:11:46 +0800

    added rotation value

--- Assets/Scripts/Bosses/Pilotras/States/BinDropState.cs
+++ Assets/Scripts/Bosses/Pilotras/States/BinDropState.cs
@@ -92,7 +92,7 @@ namespace Bosses.Pilotras.FSM
         public void SendShockwave(Vector2 center, Vector2 detectionSize)
         {
             Collider2D[] hits = Physics2D.OverlapBoxAll(center, detectionSize, 
-                character.behaviourData.drop_detection_mask);
+                0f, character.behaviourData.drop_detection_mask);
             Rigidbody2D hitRB;
 
             // apply knockback to hit NPCs
