51838402a1c600f7e6652a02e9df322fbf476767
Samuel Ting <tingjs05@gmail.com>
2024-09-02 18:09:53 +0800

    continued work, created object to store button and profile script

--- Assets/Scripts/UI/Gameplay/PauseDetailsMenu.cs
+++ Assets/Scripts/UI/Gameplay/PauseDetailsMenu.cs
@@ -1,39 +1,96 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UI.LevelSelection.CharacterSelection;
 
 namespace UI
 {
     public class PauseDetailsMenu : MonoBehaviour
     {
+        [Header("Menu Management")]
         [SerializeField] GameObject defaultPauseMenu;
+
+        [Header("Character Details View")]
+        [SerializeField] Image profileImage;
+        [SerializeField] TextMeshProUGUI characterName;
+        [SerializeField] TextMeshProUGUI characterDesc;
 
         [Header("Character Select Profile")]
         [SerializeField] Transform profileParent;
         [SerializeField] GameObject profilePrefab;
 
-        // Start is called before the first frame update
-        void Start()
+        List<ProfileObject> profileObjects = new List<ProfileObject>();
+        class ProfileObject
         {
-            
+            public CharacterSelectProfile profile;
+            public Button button;
         }
 
-        // Update is called once per frame
-        void Update()
-        {
-            
-        }
+        PlayerCharacterSO[] characterInfo => GameManager.Instance.selectedCharacters;
 
+        #region Handle Active
+        /// <summary>
+        /// Show pause details menu
+        /// </summary>
         public void Activate()
         {
             gameObject.SetActive(true);
             defaultPauseMenu.SetActive(false);
+            LoadProfileObjects();
         }
 
+        /// <summary>
+        /// Hide pause details menu
+        /// </summary>
         public void Deactivate()
         {
             gameObject.SetActive(false);
             defaultPauseMenu.SetActive(true);
         }
+        #endregion
+
+        #region Button Handlers
+        public void ShowCharacter(CharacterSelectProfile ctx)
+        {
+
+        }
+        #endregion
+
+        #region Private Methods
+        void LoadProfileObjects()
+        {
+            foreach (ProfileObject profileObject in profileObjects)
+            {
+                profileObject.profile?.gameObject.SetActive(false);
+            }
+
+            for (int i = 0; i < characterInfo.Length; i++)
+            {
+                CharacterSelectProfile selectedProfile;
+                
+                if (i >= profileObjects.Count)
+                {
+                    GameObject newObject = Instantiate(profilePrefab, profileParent);
+                    ProfileObject newProfile = new ProfileObject 
+                    {
+                        profile = newObject.GetComponent<CharacterSelectProfile>(),
+                        button = newObject.GetComponent<Button>()
+                    };
+                    profileObjects.Add(newProfile);
+                    selectedProfile = profileObjects[^1].profile;
+                }
+                else 
+                {
+                    selectedProfile = profileObjects[i].profile;
+                }
+
+                if (selectedProfile == null) continue;
+                selectedProfile.gameObject.SetActive(true);
+                selectedProfile.SetCharacter(characterInfo[i]);
+                selectedProfile.HideBorder();
+            }
+        }
+        #endregion
     }
 }
